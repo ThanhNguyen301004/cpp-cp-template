599ec506dacb20bd585ecef4646fc32ad09a7e6a
Van Thanh Nguyen <151557055+ThanhNguyen301004@users.noreply.github.com>
2026-03-10 20:29:14 +0700

    Add template for C++ competitive programming

---
+++ template.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll; typedef long double ld;
+typedef double db; typedef string str; 
+ 
+typedef pair<int,int> pi; typedef pair<ll,ll> pl; 
+typedef pair<db,db> pd; typedef vector<int> vi; 
+typedef vector<ll> vl; typedef vector<db> vd; 
+typedef vector<str> vs; typedef vector<pi> vpi;
+typedef vector<pl> vpl; typedef vector<pd> vpd; 
+#define mp make_pair
+#define f first
+#define s second
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend() 
+#define rsz resize
+#define ins insert 
+#define ft front() 
+#define bk back()
+#define pf push_front 
+#define pb push_back
+#define eb emplace_back 
+#define lb lower_bound 
+#define ub upper_bound 
+ 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(x,a) for (auto& x: a)
+ 
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5; 
+const ll INF = 1e18; 
+const ld PI = acos((ld)-1);
+const int xd[4] = {1,0,-1,0}, yd[4] = {0,1,0,-1}; 
+
+main() {		
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	ios::sync_with_stdio(false); cin.tie(0);
+}
